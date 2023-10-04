879fcf8884f0be1075bcde1e637db8b744b70f2b
Tomas Bravo <tomas.b.c@outlook.com>
2023-10-04 10:51:48 -0300

    agregando skeleton a frontedn

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,16 @@
+import { join } from 'path';
+import { skeleton } from '@skeletonlabs/skeleton';
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+	darkMode: 'class',
+	content: [
+		'./src/**/*.{html,js,svelte,ts}',
+		join(require.resolve('@skeletonlabs/skeleton'), '../**/*.{html,js,svelte,ts}')
+	],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [
+		skeleton
+	]
 };
