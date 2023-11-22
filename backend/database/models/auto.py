e96292fd3a5466f18218c905b329ff5658f8048d
JGomez1423 <JGomez2019@udec.cl>
2023-11-22 04:01:28 -0300

    se añaden comentarios a los codigos de autos

--- backend/database/models/auto.py
+++ backend/database/models/auto.py
@@ -1,8 +1,21 @@
+#imports de bibliotecas
 from pydantic import BaseModel, Field
 from typing import List
+
+#imports de otros archivos del proyecto
 from models.persona import Persona
+
 class Auto(BaseModel):
+    # Definición de los campos del modelo Auto
+
+    # Campo para la patente del auto
     patente: str
+
+    # Campo para el modelo del auto
     modelo: str
+
+    # Campo para el año del auto
     año: int
+
+    # Campo para el rut del propietario del auto, esto sera para buscar el resto de informacion del propietario en el post
     rut: str
--- backend/database/routes/auto.py
+++ backend/database/routes/auto.py
@@ -1,85 +1,92 @@
+#Bibliotecas a importar
 from fastapi import APIRouter,status,Response
 from passlib.hash import sha256_crypt ##pa los usuarios (preguntar si auto == usuario)
 from bson import ObjectId
 from starlette.status import HTTP_204_NO_CONTENT
 import json 
+#Importamos desde otros archivos del proyecto
 from config.db import con
 from schemas.auto import autoEntity, autosEntity
 from models.auto import Auto
 from models.persona import Persona
 from schemas.persona import personaEntity, personasEntity
 
-
+#llamamos a la funcion para rutas de fastAPI y la almacenamos en auto pues estas seran las rutas relacionadas con esa coleccion
 auto = APIRouter()
-
+#El endpoint para obtener todos los autos
 @auto.get('/autos')
 async def find_all_autos():
-     return autosEntity(con.test.auto.find())
+     return autosEntity(con.test.auto.find())#retornamos una autosEntity con el resultlado de encontrar todos los autos
 
-@auto.get('/autos/{id}')
+
+#enpoint para obtener un auto dado un id 
+@auto.get('/autos/{id}')#pedimos el id
 async def find_auto(id: str):
-    return autoEntity(con.test.auto.find_one({"_id": ObjectId(id)}))
+    return autoEntity(con.test.auto.find_one({"_id": ObjectId(id)}))#retornamos un autoEntity con el resultado de la query de buscar un auto dado un ID
 
+#enpoint para obtener un auto dado una patente
 @auto.get('/autos/patente/{patente}')
-async def find_auto(patente: str):
-    return autoEntity(con.test.auto.find_one({"patente": patente}))
+async def find_auto(patente: str):#pedimos la patente
+    return autoEntity(con.test.auto.find_one({"patente": patente}))#retornamos un autoEntity con el resultado de la query de buscar un auto dado una patente
     
 
-
+#endpoint para ingresar un auto a la base de datos
 @auto.post('/autos')
-async def create_auto(auto: Auto):
-    rut_propietario = auto.rut
-    persona_data = con.test.persona.find_one({"rut": rut_propietario})
-    
-    print("Tipo de persona_data:", type(persona_data))
-    print("persona_data:", persona_data)
+async def create_auto(auto: Auto):#pedimos el objeto auto
+    rut_propietario = auto.rut#extraemos de este el rut de su propietario
+    persona_data = con.test.persona.find_one({"rut": rut_propietario})#hacemos una query que busca una persona dado un rut y guardamos el resultado en persona_data
 
-    if not persona_data:
+    if not persona_data:#si no existe entonces el propietario del auto que se quiere asignar, no existe por lo que se manda una alerta
         raise HTTPException(status_code=404, detail="Propietario no encontrado")
 
-    # Crear un objeto Persona directamente con los datos necesarios
+    # Luego se crea un objeto Persona directamente con los datos necesarios
     propietario = Persona(
-        id=str(persona_data["_id"]),  # Convertir el ObjectId a una cadena
+        id=str(persona_data["_id"]),  # Convertimos el ObjectId a una cadena
         rut=persona_data["rut"],
         nombre=persona_data["nombre"],
         rol=persona_data["rol"]
     )
 
-    # Convertir el objeto Persona a un diccionario usando el método dict() de Pydantic
+    # Convertimos el objeto propietario a un diccionario usando el método dict() de Pydantic
     propietario_dict = propietario.dict()
     
+    #generamos el objeto auto con los campos necesarios
     new_auto = {
         "patente": auto.patente,
         "modelo": auto.modelo,
         "año": auto.año,
         "propietario": propietario_dict
     }
-    print("propietario: ", new_auto["propietario"])
+    
+    #insertamos en la coleccion el auto generado 
     auto_id = con.test.auto.insert_one(new_auto).inserted_id
 
+    #retornamos el id a modo de confirmacion
     return autoEntity(con.test.auto.find_one({"_id": ObjectId(auto_id)}))
 
 
-
+#endpoint para modificar un auto dado su id
 @auto.put('/autos/{id}')
-async def update_auto(id: str, auto: Auto):
-    con.test.auto.find_one_and_update({
+async def update_auto(id: str, auto: Auto): #solicitamos el id y el nuevo contenido
+    con.test.auto.find_one_and_update({#llamamos al metodo find_one_and_update para encontrar un auto con el id dado y reemplazar sus datos por los ingresados
         "_id": ObjectId(id)
     }, {
         "$set": dict(auto)
     })
-    return autoEntity(con.test.auto.find_one({"_id": ObjectId(id)}))
+    return autoEntity(con.test.auto.find_one({"_id": ObjectId(id)}))#retornamos el id del auto a modo de confirmacion
 
+#endpoint para eliminar un auto
 @auto.delete('/autos/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
-async def delete_auto(id: str):
-    con.test.auto.find_one_and_delete({
+async def delete_auto(id: str):#pedimos un id
+    con.test.auto.find_one_and_delete({#llamamos a la funcion find_one_and_delete para buscar y eliminar el auto con el id dado
         "_id": ObjectId(id)
     })
-    return Response(status_code=HTTP_204_NO_CONTENT)
+    return Response(status_code=HTTP_204_NO_CONTENT)#retornamos una respuesta http
     
+#endpoint para obtener todas las patentes de los autos ingresados    
 @auto.get('/autos_plates')
 async def get_plates():
-    autos_documents = con.test.auto.find()
-    autos_list = autosEntity(autos_documents)
-    plates = [auto["patente"] for auto in autos_list]
-    return plates
+    autos_documents = con.test.auto.find()#hacemos un find de todos los autos
+    autos_list = autosEntity(autos_documents)#los convertimos en una autosEntity
+    plates = [auto["patente"] for auto in autos_list]#extraemos de ahi las patentes
+    return plates#las retornamos
