bf783a70076f5708eabcdff06e43fc3f2cabe779
JGomez1423 <JGomez2019@udec.cl>
2023-11-23 18:53:52 -0300

    arreglando endpoin de horario y agregando comentarios

--- backend/database/routes/horario.py
+++ backend/database/routes/horario.py
@@ -1,36 +1,42 @@
+#Bibliotecas a importar
 from fastapi import APIRouter,status,Response,HTTPException
 from passlib.hash import sha256_crypt ##pa los usuarios (preguntar si horario == usuario)
 from bson import ObjectId
 from starlette.status import HTTP_204_NO_CONTENT
 from datetime import datetime, timedelta
-
+#imports de otros archivos
 from config.db import con
 from schemas.horario import horarioEntity, horariosEntity
 from models.horario import Horario
 
 
-horario = APIRouter()
+#llamamos a la funcion para rutas de fastAPI y la almacenamos en horario pues estas seran las rutas relacionadas con esa coleccion
+horario  = APIRouter()
 
+#El endpoint para obtener todos horarios recordar que llamamos "horario" a las ocurrencias de un vehiculo detectado por las camaras 
 @horario.get('/horarios')
 async def find_all_horarios():
-     return horariosEntity(con.test.horario.find())
+     return horariosEntity(con.test.horario.find())#Devolvemos un horariosEntity con todos los horarios en la base de datos
 
+#Endpoint para obtener todos los horarios que tengan una licencia especifica en un timestamp de hace 30min hasta ahora
 @horario.get('/horarios/30minuteTimeRange/{licence}/{timestamp}')
 async def find_horarios_in_range(licence: str, timestamp: int):
     return horariosEntity(con.test.horario.find(
         {
             "licence": licence,
             "time" :{
-                '$gte': (datetime.fromtimestamp(timestamp) - timedelta(minutes=300)),
+                '$gte': (datetime.fromtimestamp(timestamp) - timedelta(minutes=300)),#buscamos un tiempo que sea hace 30min con el time delta
                 '$lte': datetime.fromtimestamp(timestamp)
             }
         }
     ))
 
+#endpoint para obtener todos los horarios dado un id
 @horario.get('/horarios/{id}') 
 async def find_horario(id: str):
-    return horarioEntity(con.test.horario.find_one({"_id": ObjectId(id)}))
-    
+    return horarioEntity(con.test.horario.find_one({"_id": ObjectId(id)})) #buscamos el horario con el id dado y lo retornamos 
+
+#endpoint que obtiene los horarios dado un car_id   
 @horario.get('/horarios/car_id/{id}')
 async def find_horario(car_id: int):
     # Buscar el horario por el campo "car_id"
@@ -43,27 +49,29 @@ async def find_horario(car_id: int):
     # Crear la entidad de horario y devolverla
     return horarioEntity(horario_document)
 
+#endpoint para ingresar un horario
 @horario.post('/horarios')
 async def create_horario(horario: Horario):
     new_horario = dict(horario)
-    # Convierte el campo 'lugar' en un diccionario
-    new_horario['lugar'] = dict(horario.lugar)
     
-    id = con.test.horario.insert_one(new_horario).inserted_id
+    id = con.test.horario.insert_one(new_horario).inserted_id#insertamos el horario en la db y retornamos su id como confirmacion
     return str(id)
 
-@horario.put('/horarios/{id}')
-async def update_horario(id: str, horario: Horario):
-    con.test.horario.find_one_and_update({
+
+#endpoint para modificar un horario dado su id
+@horario.put('/horarios/{id}')#
+async def update_horario(id: str, horario: Horario):#solicitamos el id y el  nuevo horario
+    con.test.horario.find_one_and_update({#usamos la funcion find_one_and_update para buscar un horario con el id dado y reemplazar sus datos por los nuevos
         "_id": ObjectId(id)
     }, {
         "$set": dict(horario)
     })
     return horarioEntity(con.test.horario.find_one({"_id": ObjectId(id)}))
 
+#endpoint para eliminar un horario dado un id
 @horario.delete('/horarios/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=["users"])
-async def delete_horario(id: str):
-    con.test.horario.find_one_and_delete({
+async def delete_horario(id: str):#solicitamos el id
+    con.test.horario.find_one_and_delete({#usamos find_one_and_delete para borrar el objeto con ese id
         "_id": ObjectId(id)
     })
     return Response(status_code=HTTP_204_NO_CONTENT)
